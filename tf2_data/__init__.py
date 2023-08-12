4b397f5fa79535c1598d2d443ca03cf6aa5549ce
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-08-11 22:26:37 -0500

    all default scout weapons added

--- tf2_data/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import lore
-from . import images
-from . import weapons
--- tf2_data/weapons/scout_weapons.py
+++ tf2_data/weapons/scout_weapons.py
@@ -6,7 +6,22 @@ class ScoutWeapons:
     return {
       'name': 'scattergun',
       'class': 'scout',
-      'type': 'default',
-      'image': url_for("static", filename="scout_images/default_scattergun.png") ## 
+      'type': 'stock',
+      'image': url_for('static', filename='scout_images/default_scattergun.png') 
       }
+    
+  def pistol():
+    return {
+      'name': 'pistol',
+      'class': ['scout', 'engineer'],
+      'type': 'stock',
+      'image': url_for('static', filename='global_weapon_images/default_pistol.png') 
+    }
   
+  def bat():
+    return {
+      'name': 'bat',
+      'class': 'scout',
+      'type': 'stock',
+      'image': url_for('static', filename='scout_images/default_bat.png')
+    }
