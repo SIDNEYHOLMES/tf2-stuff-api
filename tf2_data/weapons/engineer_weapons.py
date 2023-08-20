c2492775e070890501de64a862fa7dd8039b6ad9
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-08-20 18:51:47 -0500

    heavy engineer and medic weapon class properties set properly

--- tf2_data/weapons/engineer_weapons.py
+++ tf2_data/weapons/engineer_weapons.py
@@ -3,61 +3,61 @@ from flask import url_for
 class EngineerWeapons:
   
   def shotgun():
-      return {"name": "Shotgun", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/5/5f/Item_icon_Shotgun.png/100px-Item_icon_Shotgun.png'}
+      return {"name": "Shotgun", 'class': ['soldier', 'pyro', 'heavy', 'engineer'], 'type': 'Stock', 'image': '/w/images/thumb/5/5f/Item_icon_Shotgun.png/100px-Item_icon_Shotgun.png'}
 
   def frontier_justice():
-      return {"name": "Frontier Justice", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/2/26/Item_icon_Frontier_Justice.png/100px-Item_icon_Frontier_Justice.png'}
+      return {"name": "Frontier Justice", 'class': 'engineer', 'type': 'Unlock', 'image': '/w/images/thumb/2/26/Item_icon_Frontier_Justice.png/100px-Item_icon_Frontier_Justice.png'}
 
   def widowmaker():
-      return {"name": "Widowmaker", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/b/b8/Item_icon_Widowmaker.png/100px-Item_icon_Widowmaker.png'}
+      return {"name": "Widowmaker", 'class': 'engineer', 'type': 'Promotional', 'image': '/w/images/thumb/b/b8/Item_icon_Widowmaker.png/100px-Item_icon_Widowmaker.png'}
 
   def pomson_6000():
-      return {"name": "Pomson 6000", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/8/89/Item_icon_Pomson_6000.png/100px-Item_icon_Pomson_6000.png'}
+      return {"name": "Pomson 6000", 'class': 'engineer', 'type': 'Craft', 'image': '/w/images/thumb/8/89/Item_icon_Pomson_6000.png/100px-Item_icon_Pomson_6000.png'}
 
   def rescue_ranger():
-      return {"name": "Rescue Ranger", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/2/29/Item_icon_Rescue_Ranger.png/100px-Item_icon_Rescue_Ranger.png'}
+      return {"name": "Rescue Ranger", 'class': 'engineer', 'type': 'Craft', 'image': '/w/images/thumb/2/29/Item_icon_Rescue_Ranger.png/100px-Item_icon_Rescue_Ranger.png'}
 
   def panic_attack():
-      return {"name": "Panic Attack", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/b/be/Item_icon_Panic_Attack.png/100px-Item_icon_Panic_Attack.png'}
+      return {"name": "Panic Attack", 'class': ['soldier', 'pyro', 'heavy', 'engineer'], 'type': 'Craft', 'image': '/w/images/thumb/b/be/Item_icon_Panic_Attack.png/100px-Item_icon_Panic_Attack.png'}
 
   def pistol():
-      return {"name": "Pistol", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/5/52/Item_icon_Pistol.png/100px-Item_icon_Pistol.png'}
+      return {"name": "Pistol", 'class': ['scout', 'engineer'], 'type': 'Stock', 'image': '/w/images/thumb/5/52/Item_icon_Pistol.png/100px-Item_icon_Pistol.png'}
 
   def lugermorph():
-      return {"name": "Lugermorph", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/8/86/Item_icon_Lugermorph.png/100px-Item_icon_Lugermorph.png'}
+      return {"name": "Lugermorph", 'class': ['scout', 'engineer'], 'type': 'Promotional', 'image': '/w/images/thumb/8/86/Item_icon_Lugermorph.png/100px-Item_icon_Lugermorph.png'}
 
   def capper():
-      return {"name": "C.A.P.P.E.R", 'class': '', 'type': 'Uncrate', 'image': '/w/images/thumb/a/a6/Item_icon_C.A.P.P.E.R.png/100px-Item_icon_C.A.P.P.E.R.png'}
+      return {"name": "C.A.P.P.E.R", 'class': ['scout', 'engineer'], 'type': 'Uncrate', 'image': '/w/images/thumb/a/a6/Item_icon_C.A.P.P.E.R.png/100px-Item_icon_C.A.P.P.E.R.png'}
 
   def wrangler():
-      return {"name": "Wrangler", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/c/ce/Item_icon_Wrangler.png/100px-Item_icon_Wrangler.png'}
+      return {"name": "Wrangler", 'class': 'engineer', 'type': 'Unlock', 'image': '/w/images/thumb/c/ce/Item_icon_Wrangler.png/100px-Item_icon_Wrangler.png'}
 
   def giger_counter():
-      return {"name": "Giger Counter", 'class': '', 'type': 'Uncrate', 'image': '/w/images/thumb/5/5d/Item_icon_Giger_Counter.png/100px-Item_icon_Giger_Counter.png'}
+      return {"name": "Giger Counter", 'class': 'engineer', 'type': 'Uncrate', 'image': '/w/images/thumb/5/5d/Item_icon_Giger_Counter.png/100px-Item_icon_Giger_Counter.png'}
 
   def short_circuit():
-      return {"name": "Short Circuit", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/3/36/Item_icon_Short_Circuit.png/100px-Item_icon_Short_Circuit.png'}
+      return {"name": "Short Circuit", 'class': 'engineer', 'type': 'Promotional', 'image': '/w/images/thumb/3/36/Item_icon_Short_Circuit.png/100px-Item_icon_Short_Circuit.png'}
 
   def wrench():
-      return {"name": "Wrench", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/7/7d/Item_icon_Wrench.png/100px-Item_icon_Wrench.png'}
+      return {"name": "Wrench", 'class': 'engineer', 'type': 'Stock', 'image': '/w/images/thumb/7/7d/Item_icon_Wrench.png/100px-Item_icon_Wrench.png'}
 
   def golden_wrench():
-      return {"name": "Golden Wrench", 'class': '', 'type': 'Distributed', 'image': '/w/images/thumb/9/95/Item_icon_Golden_Wrench.png/100px-Item_icon_Golden_Wrench.png'}
+      return {"name": "Golden Wrench", 'class': 'engineer', 'type': 'Distributed', 'image': '/w/images/thumb/9/95/Item_icon_Golden_Wrench.png/100px-Item_icon_Golden_Wrench.png'}
 
   def gunslinger():
-      return {"name": "Gunslinger", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/c/ca/Item_icon_Gunslinger.png/100px-Item_icon_Gunslinger.png'}
+      return {"name": "Gunslinger", 'class': 'engineer', 'type': 'Unlock', 'image': '/w/images/thumb/c/ca/Item_icon_Gunslinger.png/100px-Item_icon_Gunslinger.png'}
 
   def southern_hospitality():
-      return {"name": "Southern Hospitality", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/e/ec/Item_icon_Southern_Hospitality.png/100px-Item_icon_Southern_Hospitality.png'}
+      return {"name": "Southern Hospitality", 'class': 'engineer', 'type': 'Craft', 'image': '/w/images/thumb/e/ec/Item_icon_Southern_Hospitality.png/100px-Item_icon_Southern_Hospitality.png'}
 
   def jag():
-      return {"name": "Jag", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/4/49/Item_icon_Jag.png/100px-Item_icon_Jag.png'}
+      return {"name": "Jag", 'class': 'engineer', 'type': 'Craft', 'image': '/w/images/thumb/4/49/Item_icon_Jag.png/100px-Item_icon_Jag.png'}
 
   def eureka_effect():
-      return {"name": "Eureka Effect", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/c/cf/Item_icon_Eureka_Effect.png/100px-Item_icon_Eureka_Effect.png'}
+      return {"name": "Eureka Effect", 'class': 'engineer', 'type': 'Craft', 'image': '/w/images/thumb/c/cf/Item_icon_Eureka_Effect.png/100px-Item_icon_Eureka_Effect.png'}
 
   def pda():
-      return {"name": "PDA", 'class': '', 'type': 'StockPDA 1', 'image': '/w/images/thumb/1/16/Item_icon_PDA_Build.png/100px-Item_icon_PDA_Build.png'}
+      return {"name": "PDA", 'class': 'engineer', 'type': 'StockPDA 1', 'image': '/w/images/thumb/1/16/Item_icon_PDA_Build.png/100px-Item_icon_PDA_Build.png'}
 
   def pda():
-      return {"name": "PDA", 'class': '', 'type': 'StockPDA 2', 'image': '/w/images/thumb/2/26/Item_icon_PDA_Destroy.png/100px-Item_icon_PDA_Destroy.png'}
+      return {"name": "PDA", 'class': 'engineer', 'type': 'StockPDA 2', 'image': '/w/images/thumb/2/26/Item_icon_PDA_Destroy.png/100px-Item_icon_PDA_Destroy.png'}
--- tf2_data/weapons/heavy_weapons.py
+++ tf2_data/weapons/heavy_weapons.py
@@ -3,73 +3,73 @@ from flask import url_for
 class HeavyWeapons: 
   
   def minigun():
-      return {"name": "Minigun", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/a/a7/Item_icon_Minigun.png/100px-Item_icon_Minigun.png'}
+      return {"name": "Minigun", 'class': 'heavy', 'type': 'Stock', 'image': '/w/images/thumb/a/a7/Item_icon_Minigun.png/100px-Item_icon_Minigun.png'}
 
   def iron_curtain():
-      return {"name": "Iron Curtain", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/d/da/Item_icon_Iron_Curtain.png/100px-Item_icon_Iron_Curtain.png'}
+      return {"name": "Iron Curtain", 'class': 'heavy', 'type': 'Promotional', 'image': '/w/images/thumb/d/da/Item_icon_Iron_Curtain.png/100px-Item_icon_Iron_Curtain.png'}
 
   def natascha():
-      return {"name": "Natascha", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/c/cc/Item_icon_Natascha.png/100px-Item_icon_Natascha.png'}
+      return {"name": "Natascha", 'class': 'heavy', 'type': 'Unlock', 'image': '/w/images/thumb/c/cc/Item_icon_Natascha.png/100px-Item_icon_Natascha.png'}
 
   def brass_beast():
-      return {"name": "Brass Beast", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/6/64/Item_icon_Brass_Beast.png/100px-Item_icon_Brass_Beast.png'}
+      return {"name": "Brass Beast", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/6/64/Item_icon_Brass_Beast.png/100px-Item_icon_Brass_Beast.png'}
 
   def tomislav():
-      return {"name": "Tomislav", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/3/3e/Item_icon_Tomislav.png/100px-Item_icon_Tomislav.png'}
+      return {"name": "Tomislav", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/3/3e/Item_icon_Tomislav.png/100px-Item_icon_Tomislav.png'}
 
   def huo_long_heater():
-      return {"name": "Huo-Long Heater", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/8/81/Item_icon_Huo-Long_Heater.png/100px-Item_icon_Huo-Long_Heater.png'}
+      return {"name": "Huo-Long Heater", 'class': 'heavy', 'type': 'Promotional', 'image': '/w/images/thumb/8/81/Item_icon_Huo-Long_Heater.png/100px-Item_icon_Huo-Long_Heater.png'}
 
   def shotgun():
-      return {"name": "Shotgun", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/5/5f/Item_icon_Shotgun.png/100px-Item_icon_Shotgun.png'}
+      return {"name": "Shotgun", 'class': ['soldier', 'pyro', 'heavy', 'engineer'], 'type': 'Stock', 'image': '/w/images/thumb/5/5f/Item_icon_Shotgun.png/100px-Item_icon_Shotgun.png'}
 
   def family_business():
-      return {"name": "Family Business", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/c/cd/Item_icon_Family_Business.png/100px-Item_icon_Family_Business.png'}
+      return {"name": "Family Business", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/c/cd/Item_icon_Family_Business.png/100px-Item_icon_Family_Business.png'}
 
   def sandvich():
-      return {"name": "Sandvich", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/5/5e/Item_icon_Sandvich.png/100px-Item_icon_Sandvich.png'}
+      return {"name": "Sandvich", 'class': 'heavy', 'type': 'Unlock', 'image': '/w/images/thumb/5/5e/Item_icon_Sandvich.png/100px-Item_icon_Sandvich.png'}
 
   def robo_sandvich():
-      return {"name": "Robo-Sandvich", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/c/cb/Item_icon_Robo-Sandvich.png/100px-Item_icon_Robo-Sandvich.png'}
+      return {"name": "Robo-Sandvich", 'class': 'heavy', 'type': 'Promotional', 'image': '/w/images/thumb/c/cb/Item_icon_Robo-Sandvich.png/100px-Item_icon_Robo-Sandvich.png'}
 
   def dalokohs_bar():
-      return {"name": "Dalokohs Bar", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/3/35/Item_icon_Dalokohs_Bar.png/100px-Item_icon_Dalokohs_Bar.png'}
+      return {"name": "Dalokohs Bar", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/3/35/Item_icon_Dalokohs_Bar.png/100px-Item_icon_Dalokohs_Bar.png'}
 
   def fishcake():
-      return {"name": "Fishcake", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/9/94/Item_icon_Fishcake.png/100px-Item_icon_Fishcake.png'}
+      return {"name": "Fishcake", 'class': 'heavy', 'type': 'Promotional', 'image': '/w/images/thumb/9/94/Item_icon_Fishcake.png/100px-Item_icon_Fishcake.png'}
 
   def buffalo_steak_sandvich():
-      return {"name": "Buffalo Steak Sandvich", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/6/62/Item_icon_Buffalo_Steak_Sandvich.png/100px-Item_icon_Buffalo_Steak_Sandvich.png'}
+      return {"name": "Buffalo Steak Sandvich", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/6/62/Item_icon_Buffalo_Steak_Sandvich.png/100px-Item_icon_Buffalo_Steak_Sandvich.png'}
 
   def panic_attack():
-      return {"name": "Panic Attack", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/b/be/Item_icon_Panic_Attack.png/100px-Item_icon_Panic_Attack.png'}
+      return {"name": "Panic Attack", 'class': ['soldier', 'pyro', 'heavy', 'engineer'], 'type': 'Craft', 'image': '/w/images/thumb/b/be/Item_icon_Panic_Attack.png/100px-Item_icon_Panic_Attack.png'}
 
   def second_banana():
-      return {"name": "Second Banana", 'class': '', 'type': 'Contract', 'image': '/w/images/thumb/7/77/Item_icon_Second_Banana.png/100px-Item_icon_Second_Banana.png'}
+      return {"name": "Second Banana", 'class': 'heavy', 'type': 'Contract', 'image': '/w/images/thumb/7/77/Item_icon_Second_Banana.png/100px-Item_icon_Second_Banana.png'}
 
   def fists():
-      return {"name": "Fists", 'class': '', 'type': 'Stock', 'image': '/w/images/thumb/1/19/Item_icon_Fists.png/100px-Item_icon_Fists.png'}
+      return {"name": "Fists", 'class': 'heavy', 'type': 'Stock', 'image': '/w/images/thumb/1/19/Item_icon_Fists.png/100px-Item_icon_Fists.png'}
 
   def apoco_fists():
-      return {"name": "Apoco-Fists", 'class': '', 'type': 'Promotional', 'image': '/w/images/thumb/9/91/Item_icon_Apoco-Fists.png/100px-Item_icon_Apoco-Fists.png'}
+      return {"name": "Apoco-Fists", 'class': 'heavy', 'type': 'Promotional', 'image': '/w/images/thumb/9/91/Item_icon_Apoco-Fists.png/100px-Item_icon_Apoco-Fists.png'}
 
   def killing_gloves_of_boxing():
-      return {"name": "Killing Gloves of Boxing", 'class': '', 'type': 'Unlock', 'image': '/w/images/thumb/f/f6/Item_icon_Killing_Gloves_of_Boxing.png/100px-Item_icon_Killing_Gloves_of_Boxing.png'}
+      return {"name": "Killing Gloves of Boxing", 'class': 'heavy', 'type': 'Unlock', 'image': '/w/images/thumb/f/f6/Item_icon_Killing_Gloves_of_Boxing.png/100px-Item_icon_Killing_Gloves_of_Boxing.png'}
 
   def gloves_of_running_urgently():
-      return {"name": "Gloves of Running Urgently", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/4/4f/Item_icon_Gloves_of_Running_Urgently.png/100px-Item_icon_Gloves_of_Running_Urgently.png'}
+      return {"name": "Gloves of Running Urgently", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/4/4f/Item_icon_Gloves_of_Running_Urgently.png/100px-Item_icon_Gloves_of_Running_Urgently.png'}
 
   def bread_bite():
-      return {"name": "Bread Bite", 'class': '', 'type': 'Uncrate', 'image': '/w/images/thumb/a/a4/Item_icon_Bread_Bite.png/100px-Item_icon_Bread_Bite.png'}
+      return {"name": "Bread Bite", 'class': 'heavy', 'type': 'Uncrate', 'image': '/w/images/thumb/a/a4/Item_icon_Bread_Bite.png/100px-Item_icon_Bread_Bite.png'}
 
   def warriors_spirit():
-      return {"name": "Warrior's Spirit", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/8/87/Item_icon_Warrior%27s_Spirit.png/100px-Item_icon_Warrior%27s_Spirit.png'}
+      return {"name": "Warrior's Spirit", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/8/87/Item_icon_Warrior%27s_Spirit.png/100px-Item_icon_Warrior%27s_Spirit.png'}
 
   def fists_of_steel():
-      return {"name": "Fists of Steel", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/9/9c/Item_icon_Fists_of_Steel.png/100px-Item_icon_Fists_of_Steel.png'}
+      return {"name": "Fists of Steel", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/9/9c/Item_icon_Fists_of_Steel.png/100px-Item_icon_Fists_of_Steel.png'}
 
   def eviction_notice():
-      return {"name": "Eviction Notice", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/6/62/Item_icon_Eviction_Notice.png/100px-Item_icon_Eviction_Notice.png'}
+      return {"name": "Eviction Notice", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/6/62/Item_icon_Eviction_Notice.png/100px-Item_icon_Eviction_Notice.png'}
 
   def holiday_punch():
-      return {"name": "Holiday Punch", 'class': '', 'type': 'Craft', 'image': '/w/images/thumb/5/54/Item_icon_Holiday_Punch.png/100px-Item_icon_Holiday_Punch.png'}
+      return {"name": "Holiday Punch", 'class': 'heavy', 'type': 'Craft', 'image': '/w/images/thumb/5/54/Item_icon_Holiday_Punch.png/100px-Item_icon_Holiday_Punch.png'}
