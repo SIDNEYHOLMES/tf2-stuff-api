faae6316169480bc2fedea32e3c12295386ec228
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2023-11-16 09:01:26 -0600

    ALL ROUTE FOR CLASS WEAPONS WORKINGgit add .git add .git add .code .

--- tf2_data/weapons/__init__.py
+++ tf2_data/weapons/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 ## IMPORT AND ADD CLASS INTO THIS FILE
 from .scout_weapons import ScoutWeapons
 from .soldier_weapons import SoldierWeapons
@@ -10,8 +12,16 @@ from .sniper_weapons import SniperWeapons
 from .spy_weapons import SpyWeapons
 from .global_weapons import GlobalWeapons
 
+## all weapons iterates through weapon models and adds each return to the result list
 def allWeapons():
-  return [ScoutWeapons, SoldierWeapons, PyroWeapons, DemomanWeapons, HeavyWeapons, EngineerWeapons, ]
+  weapon_method_list = [ScoutWeapons, SoldierWeapons, PyroWeapons, DemomanWeapons, HeavyWeapons, EngineerWeapons, MedicWeapons, SniperWeapons, SpyWeapons, GlobalWeapons]
+  result_method_list = []
+  while (len(result_method_list) < 10):
+    curr_weapon = weapon_method_list[len(result_method_list)]
+    result_method_list.append(curr_weapon)
+  
+  # print(result_method_list)
+  return result_method_list
 
 ALL_WEAPON_CLASSES = {
   "scout": ScoutWeapons,
